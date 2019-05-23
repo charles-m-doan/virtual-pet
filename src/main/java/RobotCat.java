fd29c79b00be5e3e2d126b095b0115470cd6ae9c
bryce-sampson <sampsonbrycea@gmail.com>
2019-05-23 10:05:44 -0400

    create values for robot-cat

---
+++ src/main/java/RobotCat.java
@@ -0,0 +1,30 @@
+public class VirtualRobotCat extends VirtualPet {
+
+	public VirtualRobotCat(String name) {
+		super(name);
+		this.hungerRate = 4;
+		this.thirstRate = 2;
+		this.boredomRate = 2;
+		this.minHungerCapacity = 0;
+		this.maxHungerCapacity = 45;
+		this.minThirstCapacity = 0;
+		this.maxThirstCapacity = 30;
+		this.minBoredomCapacity = 0;
+		this.maxBoredomCapacity = 100;
+
+		this.hunger = getValueBetweenRange(45, maxHungerCapacity);
+		this.thirst = getValueBetweenRange(30, maxThirstCapacity);
+		this.boredom = getValueBetweenRange(100, maxBoredomCapacity);
+	}
+
+	public String getType() {
+		return "RobotCat";
+	}
+
+	public void tick() {
+		decreaseHunger(hungerRate);
+		decreaseThirst(thirstRate);
+		decreaseBoredom(boredomRate);
+	}
+
+}
