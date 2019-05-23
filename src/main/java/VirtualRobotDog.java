563e7c36f5718675a97b9b17f4db6c226ab77c22
tyler-conner <tyler.conner@gmail.com>
2019-05-23 09:56:14 -0400

    Created robot dog class

---
+++ src/main/java/VirtualRobotDog.java
@@ -0,0 +1,30 @@
+public class VirtualRobotDog extends VirtualPet {
+
+	public VirtualRobotDog(String name) {
+		super(name);
+		this.hungerRate = 2;
+		this.thirstRate = 1;
+		this.boredomRate = 1;
+		this.minHungerCapacity = 0;
+		this.maxHungerCapacity = 45;
+		this.minThirstCapacity = 0;
+		this.maxThirstCapacity = 30;
+		this.minBoredomCapacity = 0;
+		this.maxBoredomCapacity = 100;
+
+		this.hunger = getValueBetweenRange(15, maxHungerCapacity);
+		this.thirst = getValueBetweenRange(10, maxThirstCapacity);
+		this.boredom = getValueBetweenRange(50, maxBoredomCapacity);
+	}
+
+	public String getType() {
+		return "Robot Dog";
+	}
+
+	public void tick() {
+		decreaseHunger(hungerRate);
+		decreaseThirst(thirstRate);
+		decreaseBoredom(boredomRate);
+	}
+
+}
