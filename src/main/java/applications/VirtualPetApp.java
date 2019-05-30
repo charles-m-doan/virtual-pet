c36a0e514206d5b6c505e93f47696305eac23b58
charles-m-doan <cmatthewdoan@gmail.com>
2019-05-30 10:18:17 -0400

    Updated app interface testing

--- src/main/java/applications/VirtualPetApp.java
+++ src/main/java/applications/VirtualPetApp.java
@@ -1,8 +1,24 @@
 package applications;
 
+import models.VirtualShelter;
+import organicpets.OrganicCat;
+import organicpets.OrganicDog;
+import robotpets.RobotCat;
+import robotpets.RobotDog;
+
 public class VirtualPetApp {
 
 	public static void main(String[] args) {		
-		
+		VirtualShelter vs = new VirtualShelter();
+		vs.addPet(new OrganicCat("Mittens"));
+		vs.addPet(new OrganicDog("Jeff"));
+		vs.addPet(new RobotDog("Kujo"));
+		vs.addPet(new RobotCat("Whiskers"));
+		vs.addPet(new OrganicCat("Ninja"));
+		vs.addPet(new OrganicDog("George"));
+		vs.addPet(new RobotDog("K-9"));
+		vs.addPet(new RobotCat("Terminator"));
+		AppInterface2 testInterface = new AppInterface2(vs);
+		testInterface.startInterface();
 	}
 }
--- src/test/java/applications/VirtualPetAppTests.java
+++ src/test/java/applications/VirtualPetAppTests.java
@@ -1,3 +1,4 @@
+package applications;
 import org.junit.Test;
 
 import models.VirtualShelter;
